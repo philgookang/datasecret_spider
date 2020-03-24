fa3474a1c4be1e12fc025f4d4599bb69a264f33c
Phil Goo Kang <philgookang@gmail.com>
2020-03-24 13:54:13 +0900

    added <> arrows to inidicate caller

--- library/Log.py
+++ library/Log.py
@@ -3,4 +3,5 @@ from datetime import datetime
 def LOG(*args):
 	args = list(args)
 	args.insert(0, "[{0}]".format(datetime.now().strftime("%y-%m-%d %H:%M:%S")) )
+	args[1] = "<{0}>".format(args[1])
 	print(*args, sep=' ')
